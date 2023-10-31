85a8b90454c5bbf440114509b4798153d0552a5c
jovispereira <pereirajovis@gmail.com>
2023-10-31 10:51:30 -0300

    Projeto v1 revisado

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
 const modalPopup = document.querySelector("#modal-popup");
 const btnFecharModal = document.querySelector(".btn-modal-popup");
 
+// Função para gerenciar o foco dentro das Modais
+function gerenciarFocoModal(modalId) {
+  const modal = document.querySelector(`#${modalId}`);
+  const elementosModal = modal.querySelectorAll('a, button, input, select, textarea, [tabindex]:not([tabindex="-1"])');
+  const primeiroElemento = elementosModal[0];
+  const ultimoElemento = elementosModal[elementosModal.length - 1];
+
+  modal.addEventListener('keydown', (event) => {
+    if (event.key === 'Tab') {
+      if (event.shiftKey) {
+        // Se a tecla Shift+Tab for pressionada, direcione o foco para o último elemento do modal
+        if (document.activeElement === primeiroElemento) {
+          event.preventDefault();
+          ultimoElemento.focus();
+        }
+      } else {
+        // Se apenas a tecla Tab for pressionada, direcione o foco para o primeiro elemento do modal
+        if (document.activeElement === ultimoElemento) {
+          event.preventDefault();
+          primeiroElemento.focus();
+        }
+      }
+    }
+  });
+}
+
 // Função para carregar a janela modal
 function abrirModal(carregarModal) {
   // Receber o seletor da janela modal que será aberta
@@ -25,14 +51,38 @@ function fecharModal(fecharModal) {
   document.body.style.overflow = 'auto';
 }
 
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    fecharModal('ver-modal');
+    fecharModal('ver-modal-enviado');
+  }
+});
+
 // Função para mostrar a modal dialog após 3 segundos
 function exibirModalAutomaticamente() {
   setTimeout(function () {
+    var modalPopup = document.getElementById('modal-popup');
     modalPopup.showModal();
     modalPopup.style.display = 'block';
     document.body.style.overflow = 'hidden';
+
+    var emailInput = modalPopup.querySelector('#email');
+    if (emailInput) {
+      emailInput.focus();
+      emailInput.select(); // Seleciona o texto no campo
+    }
   }, 3000);
 }
+
+// Fechar a modal ao pressionar ESC
+modalPopup.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+   modalPopup.close();
+
+  modalPopup.style.display = 'none';
+  document.body.style.overflow = 'auto';
+  }
+});
 
 btnFecharModal.onclick = function () {
   modalPopup.close();
@@ -41,15 +91,17 @@ btnFecharModal.onclick = function () {
   document.body.style.overflow = 'auto';
 }
 
-// Função para abrir a modal de inscrição
 function inscrever() {
   // Fechar a modal de diálogo
   modalPopup.close();
-  
-  // Abrir a modal de inscrição
-  const modalEnviado = document.getElementById('ver-modal-enviado');
-  modalEnviado.showModal();
+
+  abrirModal('ver-modal-inscrito')
 }
 
 // Chama a função para exibir a modal automaticamente
 exibirModalAutomaticamente();
+
+// Chamar essa função para cada modal que você deseja controlar o foco
+gerenciarFocoModal('ver-modal');
+gerenciarFocoModal('ver-modal-enviado');
+gerenciarFocoModal('modal-popup');
